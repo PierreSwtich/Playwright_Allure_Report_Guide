c32604ca0de84bccb1f5098ff5572c5851df1caf
Pierre Swtich <a.stolarski.as@gmail.com>
2023-01-04 17:42:14 +0100

    Install Playwright & Add yml configuration

---
+++ playwright.config.js
@@ -0,0 +1,35 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+};
+
+module.exports = config;
