fddd512f316544e996fbffbe6dd9759976f8eef0
PierreSwtich <skamper28@gmail.com>
2023-01-05 07:40:48 +0100

    yml Fix + added target url & playwright reporter added

--- playwright.config.js
+++ playwright.config.js
@@ -10,15 +10,14 @@ const config = {
   expect: {
     timeout: 5000
   },
-
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ['line'], 
+    ['allure-playwright']
+  ],
   use: {
     actionTimeout: 0,
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
